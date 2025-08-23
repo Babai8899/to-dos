d2f8cee655c17fa8eed801d28ee2eac2f243b03e
Babai8899 <135723159+Babai8899@users.noreply.github.com>
2025-08-23 07:35:56 +0530

    Created note frontend

--- client/src/modules/todos/note/CreateNote.jsx
+++ client/src/modules/todos/note/CreateNote.jsx
@@ -1,26 +1,77 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import Transitions from '../../../components/Transitions'
+import axiosInstance from '../../../api/axiosInstance';
+import AuthContext from '../../../hooks/AuthContext';
+import ToastContext from '../../../hooks/ToastContext';
 
 function CreateNote() {
+  const {user} = useContext(AuthContext);
+  const {showToast} = useContext(ToastContext);
   const pageVariants = {
     initial: { opacity: 0, y: 50 },
     animate: { opacity: 1, y: 0 },
     exit: { opacity: 0, y: -50 },
   };
+  const [taskData, setTaskData] = useState({
+    title: "",
+    description: "",
+  });
+
+  const {
+    title,
+    description,
+  } = taskData;
+
+  const handleChange = (e) => {
+    setTaskData({
+      ...taskData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const onReset = () => {
+    setTaskData({
+      title: "",
+      description: "",
+    });
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    //add user.email to taskData
+    taskData.user = user.emailId;
+    try {
+      await axiosInstance.post('/tasks', taskData);
+      showToast("Task created successfully", "success");
+    } catch (error) {
+      console.error("Registration failed:", error.response.data.message);
+      showToast(error.response.data.message, "error");
+      throw error;
+    }
+    // Reset form after submission
+    setTimeout(() => {
+      onReset();
+    }, 1000);
+
+  }
   return (
     <Transitions pageVariants={pageVariants}>
-      <div className="w-full rounded-lg p-4">
-        <div className="container flex justify-center mx-auto my-0.5">
-          <div className="card w-96 bg-base-100 shadow-sm border-2 border-base-300 mx-auto my-5 h-96 flex flex-col justify-center items-center gap-4 py-1">
-            <h1 className='text-2xl font-bold'>Login Page</h1>
-            <label>Email ID</label>
-            <input type="text" placeholder="Email ID" className="input input-warning" name='emailId' />
-            <label>Password</label>
-            <input type="password" placeholder="Password" className="input input-warning" name='password' />
-            <div className="flex justify-center w-full mx-auto my-10 gap-5">
-              <a className="bg-base-300 rounded-box grid h-10 w-32 place-items-center cursor-pointer hover:bg-amber-100 ease-in-out transition-colors duration-300" onClick={(e) => handleSubmit(e)}>Login</a>
-              <a className="bg-base-300 rounded-box grid h-10 w-32 place-items-center cursor-pointer hover:bg-amber-100 ease-in-out transition-colors duration-300">Reset</a>
+      <div className="container flex justify-center w-1/3 mx-auto my-0.5 h-max">
+        <div className="card w-full h-96 bg-base-100 shadow-sm border-2 border-base-300 mx-auto my-5 flex flex-col justify-center items-center gap-4 py-1 px-5">
+          <h1 className='text-2xl'>Create Task</h1>
+          <div className='grid grid-cols-1 gap-0.5'>
+            <div className='w-96 max-w-xs'>
+              <input type="text" placeholder="Task title" className="input input-ghost" name='title' value={title} onChange={handleChange} />
             </div>
+            <div className='divider m-0 p-0'></div>
+            <div className='w-full max-w-xs'>
+              <textarea className="textarea textarea-ghost h-44" placeholder="Description" name='description' value={description} onChange={handleChange}></textarea>
+            </div>
+          </div>
+          <div className="flex justify-center w-full mx-auto my-2 gap-5">
+            <a className="bg-accent text-base-200 rounded-box grid h-10 w-32 place-items-center cursor-pointer hover:bg-accent/50 hover:text-accent ease-in-out transition-colors duration-300" onClick={(e) => handleSubmit(e)}>Create Task</a>
+            <a className="bg-accent text-base-200 rounded-box grid h-10 w-32 place-items-center cursor-pointer hover:bg-accent/50 hover:text-accent ease-in-out transition-colors duration-300" onClick={() => onReset}>Cancel</a>
           </div>
         </div>
       </div>
