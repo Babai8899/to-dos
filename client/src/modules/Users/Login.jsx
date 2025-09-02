e6eebd94d7e4a5056d8150a5a0ada47d2eefbf55
Babai8899 <135723159+Babai8899@users.noreply.github.com>
2025-09-02 20:23:38 +0530

    border removed for mobile view

--- client/src/modules/Users/Login.jsx
+++ client/src/modules/Users/Login.jsx
@@ -73,7 +73,7 @@ function Login() {
 
     return (
         <Transitions pageVariants={pageVariants}>
-            <div className=' my-1.5 md:w-1/2 w-screen md:h-[calc(100vh-8rem)] h-[calc(100vh-15rem)] rounded-lg flex flex-col gap-4 justify-center items-center mx-auto border-2 md:bg-yellow-50/50 md:dark:bg-cyan-900/50 md:shadow-sm md:border-2 md:border-yellow-300 md:dark:border-cyan-500'>
+            <div className=' my-1.5 md:w-1/2 w-screen md:h-[calc(100vh-8rem)] h-[calc(100vh-15rem)] rounded-lg flex flex-col gap-4 justify-center items-center mx-auto md:border-2 md:bg-yellow-50/50 md:dark:bg-cyan-900/50 md:shadow-sm md:border-2 md:border-yellow-300 md:dark:border-cyan-500'>
                     <h1 className='text-4xl text-yellow-600 dark:text-cyan-500 font-bold'>Login</h1>
                     <div className='gap-2 w-full max-w-xs'>
                         <label className='text-gray-900 dark:text-gray-200'>Email ID</label>
